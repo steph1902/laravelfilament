e274d86fc1217fb3e813dcb0744669dea357eed6
steph1902 <stephanus1902@gmail.com>
2024-09-04 15:45:24 +0700

    finished home page

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+@section('content')
+
+{{-- <h1>WELCOME</h1> --}}
+
+<style>
+    /* Customize styles as needed */
+
+body {
+    font-family: Arial, sans-serif;
+}
+
+.hero-section-1 {
+    text-align: center;
+    padding: 100px 0;
+}
+
+.hero-section-2 {
+    text-align: center;
+    padding-top: 600px;
+}
+
+.hero-section-3 {
+    text-align: center;
+    padding-top: 600px;
+}
+.link-text
+{
+    font-size: 1rem;
+    padding-left:15px;
+}
+
+</style>
+
+
+    <main class="hero-section-1">
+        <div style="margin-top:20%;"></div>
+        <div class="hero-content">
+            <h3 style="color: white !important;">MUGLER GENTLE MONSTER</h3>
+            {{-- <a href="#" class="btn btn-primary btn-lg" style="color: white !important;">Shop the Collection</a> --}}
+            <a class="link-text" href="#" class="btn-lg" style="color: white !important;">Shop the Collection</a>
+            <a class="link-text" href="#" class="btn-lg" style="color: white !important;">Visit pop-up store</a>
+        </div>
+    </main>
+
+    <main class="hero-section-2">
+        <div style="margin-top:1%;"></div>
+        <div class="hero-content">
+            <h3 style="color: white !important;">MUGLER GENTLE MONSTER</h3>
+            {{-- <a href="#" class="btn btn-primary btn-lg" style="color: white !important;">Shop the Collection</a> --}}
+            <a class="link-text" href="#" class="btn-lg" style="color: white !important;">Shop the Collection</a>
+            <a class="link-text" href="#" class="btn-lg" style="color: white !important;">Visit pop-up store</a>
+        </div>
+    </main>
+
+    <main class="hero-section-3">
+        <div style="margin-top:0.001%;"></div>
+        <div class="hero-content">
+            <h3 style="color: white !important;">MUGLER GENTLE MONSTER</h3>
+            {{-- <a href="#" class="btn btn-primary btn-lg" style="color: white !important;">Shop the Collection</a> --}}
+            <a class="link-text" href="#" class="btn-lg" style="color: white !important;">Shop the Collection</a>
+            <a class="link-text" href="#" class="btn-lg" style="color: white !important;">Visit pop-up store</a>
+        </div>
+    </main>
+
+
+
+
+
+
+@endsection
